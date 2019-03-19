a4939eae2244a26d1eef96e21bdd35dbb96948ef
Carl Tablante <carl.q.tablante@gmail.com>
2019-03-19 10:43:02 +0800

    Change baseURL based on current environment

--- src/Requests/apiRoutes.js
+++ src/Requests/apiRoutes.js
@@ -1,4 +1,11 @@
-const baseURL = "http://localhost:8000"
+let baseURL;
+
+if (process.env.NODE_ENV === 'production') {
+  baseURL = "https://sheltered-river-82974.herokuapp.com"
+} else {
+  baseURL = "http://localhost:8000"
+}
+
 const getMenu = `${baseURL}/menu`
 const cartUrl = `${baseURL}/cart_items`
 
